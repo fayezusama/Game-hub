6e5e837472ab4983e2cd39e0168425a07ebf1b20
fayezusama <fayez_usama@hotmail.com>
2023-03-20 12:03:56 +0400

    Built a navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
@@ -8,14 +9,14 @@ function App() {
         base: `"nav""main"`,
         lg: `"nav nav""aside main"`,
       }}
-      gridTemplateRows={"50px 1fr 30px"}
+      gridTemplateRows={"60px 1fr 30px"}
       gridTemplateColumns={"150px 1fr"}
       h="100vh"
       color="blackAlpha.700"
       fontWeight="bold"
     >
-      <GridItem pl="2" bg="orange.300" area={"nav"}>
-        nav
+      <GridItem pl="2" area={"nav"}>
+       <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem pl="2" bg="pink.300" area={"aside"}>
